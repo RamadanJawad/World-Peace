53c95c67be14885ba1c7ed9beb1c56ae4cac1553
Ramadan Ghattas <88488032+RamadanJawad@users.noreply.github.com>
2023-03-28 21:57:45 +0300

    change file name

--- lib/view/screen/auth/launch_screen.dart
+++ lib/view/screen/auth/launch_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:world_peace/core/constant/color.dart';
 import 'package:world_peace/view/widget/auth/bodyLaunchScreen.dart';
-
 import '../../../controller/auth/launch_controller.dart';
 
 class LaunchScreen extends StatelessWidget {
@@ -11,8 +10,9 @@ class LaunchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     LaunchController controller = Get.put(LaunchController());
-
     return const Scaffold(
-        backgroundColor: AppColor.backgroundColor, body: BodyLaunch());
+      backgroundColor: AppColor.backgroundColor,
+      body: BodyLaunch(),
+    );
   }
 }
