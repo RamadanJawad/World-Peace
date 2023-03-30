fe5176ad8d089024a8509d115f8e7e413b57c392
Ramadan Ghattas <88488032+RamadanJawad@users.noreply.github.com>
2023-03-30 18:18:48 +0300

    add url api

---
+++ lib/core/api/api_setting.dart
@@ -0,0 +1,7 @@
+class ApiSetting{
+  ApiSetting._();
+
+  static const String baseUrl='https://aqsacommuinty.health-optima.com/mobile/api/';
+  static const String login='${baseUrl}auth/login';
+  static const String register='${baseUrl}auth/register';
+}
