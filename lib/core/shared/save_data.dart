cacad98c89003d5355931eb47e5a6c81fa6addaf
Ramadan Ghattas <88488032+RamadanJawad@users.noreply.github.com>
2023-03-30 18:18:58 +0300

    save user data

---
+++ lib/core/shared/save_data.dart
@@ -0,0 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:world_peace/model/user.dart';
+
+class AppPreferences {
+  static AppPreferences? _instance;
+  late SharedPreferences sharedPreferences;
+  AppPreferences._();
+
+  factory AppPreferences() {
+    return _instance ??= AppPreferences._();
+  }
+
+  Future initSharedPreferences() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  Future save(User user) async {
+    await sharedPreferences.setBool("isLogged", true);
+    await sharedPreferences.setString("name", user.name);
+    await sharedPreferences.setString("name", user.email);
+    await sharedPreferences.setString("token", "Bearer ${user.token}");
+  }
+
+  bool get loggedIn => sharedPreferences.getBool("isLogged") ?? false;
+
+  String get token => sharedPreferences.getString("token") ?? "";
+}
