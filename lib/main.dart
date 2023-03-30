1580f9fc91a4e5a6857e0c197b8ef51dd65bbb73
Ramadan Ghattas <88488032+RamadanJawad@users.noreply.github.com>
2023-03-30 16:41:35 +0300

    init AppPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:world_peace/core/shared/save_data.dart';
 import 'package:world_peace/view/screen/auth/splash_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppPreferences().initSharedPreferences();
   runApp(const MyApp());
 }
 
