d005571c2678639bc405ad874c412bbe0f23ff4a
Ramadan Ghattas <88488032+RamadanJawad@users.noreply.github.com>
2023-04-19 01:45:11 +0300

    adding createPost function

--- lib/core/api/api_post.dart
+++ lib/core/api/api_post.dart
@@ -1,2 +1,23 @@
-class ApiPostController{
+import 'package:http/http.dart' as http;
+import 'package:world_peace/core/api/api_helper.dart';
+import 'package:world_peace/core/api/api_setting.dart';
+
+class ApiPostController with ApiHelper {
+  Future<bool> createPost(
+      {required String description, String? imagePath}) async {
+    var response = await http.post(Uri.parse(ApiSetting.createPost),
+        body: {
+          "title": "test",
+          "description": description,
+          "category_id": "1",
+          "status": "1",
+          "image": ""
+        },
+        headers: headers);
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
