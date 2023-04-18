6332400312c671f2799bb1340e6614b339137efb
Ramadan Ghattas <88488032+RamadanJawad@users.noreply.github.com>
2023-04-19 01:45:37 +0300

    adding createPost as url

--- lib/core/api/api_setting.dart
+++ lib/core/api/api_setting.dart
@@ -4,4 +4,5 @@ class ApiSetting{
   static const String baseUrl='https://aqsacommuinty.health-optima.com/mobile/api/';
   static const String login='${baseUrl}auth/login';
   static const String register='${baseUrl}auth/register';
+  static const String createPost='${baseUrl}posts/create';
 }
